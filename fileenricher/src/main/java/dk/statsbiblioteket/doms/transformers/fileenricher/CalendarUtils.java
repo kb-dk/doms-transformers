a6af89050b509e10b2819d9d88d129104670020a
Kim Teglgaard Christensen <ktc@statsbiblioteket.dk>
2012-07-17 10:45:32 +0000

    Add simple CalendarUtils class and basic implementation of transport stream filename decoder

---
+++ fileenricher/src/main/java/dk/statsbiblioteket/doms/transformers/fileenricher/CalendarUtils.java
@@ -0,0 +1,38 @@
+package dk.statsbiblioteket.doms.transformers.fileenricher;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class CalendarUtils {
+
+    /**
+     * Private constructor. 
+     */
+    private CalendarUtils() {}
+    
+
+    /**
+     * Turns a string representation of unix time into a XMLGregorianCalendar.
+     * @param unixTime The unixtime. If the argument is null, then epoch is returned.
+     * @return The XMLGregorianCalendar.
+     */
+    public static XMLGregorianCalendar getXmlGregorianCalendar(String unixTime) {
+        Date date;
+        if(unixTime == null) {
+            date = new Date(0);
+        } else {
+            date = new Date(Long.parseLong(unixTime)*1000);
+        }
+        
+        GregorianCalendar gc = new GregorianCalendar();
+        try {
+            gc.setTime(date);
+            return DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not convert the date '" + date + "' into the xml format.", e);
+        }
+    }
+}
