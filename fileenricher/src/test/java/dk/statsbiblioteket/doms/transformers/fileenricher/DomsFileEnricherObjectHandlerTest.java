630c6e01ce0e39299c5ee030a27ba05016e77ab8
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-07-17 10:43:32 +0000

    Test added

---
+++ fileenricher/src/test/java/dk/statsbiblioteket/doms/transformers/fileenricher/DomsFileEnricherObjectHandlerTest.java
@@ -0,0 +1,29 @@
+package dk.statsbiblioteket.doms.transformers.fileenricher;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: 7/17/12
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DomsFileEnricherObjectHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testTransform() throws Exception {
+
+    }
+}
