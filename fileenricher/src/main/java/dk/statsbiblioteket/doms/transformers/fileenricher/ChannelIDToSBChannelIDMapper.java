c3ad6f51dc732381b617f4715c61b9f54486886a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-07-17 09:52:59 +0000

    ChannelIDMapper

---
+++ fileenricher/src/main/java/dk/statsbiblioteket/doms/transformers/fileenricher/ChannelIDToSBChannelIDMapper.java
@@ -0,0 +1,37 @@
+package dk.statsbiblioteket.doms.transformers.fileenricher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: 7/17/12
+ * Time: 11:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChannelIDToSBChannelIDMapper {
+
+    private static ChannelIDToSBChannelIDMapper instance;
+
+    public synchronized static ChannelIDToSBChannelIDMapper getInstance(){
+        if (instance == null){
+            instance = new ChannelIDToSBChannelIDMapper();
+        }
+        return instance;
+
+    }
+
+    public String mapToSBChannelID(String otherChannelID){
+        //TODO not mockup
+        return otherChannelID;
+    }
+
+    public List<String> mapToSBChannelIDs(List<String> otherChannelIDs){
+        List<String> result = new ArrayList<String>(otherChannelIDs.size());
+        for (String otherChannelID : otherChannelIDs) {
+            result.add(mapToSBChannelID(otherChannelID));
+        }
+        return result;
+    }
+}
