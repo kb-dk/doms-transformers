c277be593474080c5ff4d36d71dc230ff005f230
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-07-19 08:29:04 +0000

    Structure for pbcore parsing

---
+++ shardmigrator/src/main/java/dk/statsbiblioteket/doms/transformers/shardmigrator/PBCoreMigrator.java
@@ -0,0 +1,29 @@
+package dk.statsbiblioteket.doms.transformers.shardmigrator;
+
+import java.io.InputStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: 7/19/12
+ * Time: 10:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PBCoreMigrator {
+
+    public PBCoreMigrator(InputStream originalPbcore) {
+    }
+
+
+    public void addGallupStructure(Gallup gallup){
+
+    }
+
+    public void addRitzauStructure(Ritzau ritzau){
+
+    }
+
+    public String toString(){
+        return null;
+    }
+}
---
+++ shardmigrator/src/main/java/dk/statsbiblioteket/doms/transformers/shardmigrator/Ritzau.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.doms.transformers.shardmigrator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: 7/19/12
+ * Time: 10:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ritzau {
+}
