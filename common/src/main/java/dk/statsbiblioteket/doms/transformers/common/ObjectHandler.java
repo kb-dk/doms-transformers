abeeb33d0431252c28f2a032b83ce48ad69f0828
Adam Tulinius <aft@statsbiblioteket.dk>
2012-11-12 14:13:09 +0100

    Document the proper return values of ObjectHandler.transform().

--- common/src/main/java/dk/statsbiblioteket/doms/transformers/common/ObjectHandler.java
+++ common/src/main/java/dk/statsbiblioteket/doms/transformers/common/ObjectHandler.java
@@ -8,6 +8,7 @@ public interface ObjectHandler {
      * Given a single object UUID, transform it.
      * @param uuid uuid of program objects.
      * @throws Exception on failure.
+     * @return COMPLETE when a transform is 100% complete, INCOMPLETE when partially complete, NOOP when the file has already been completed, FAILED when the transform failed, and IGNORED when the transformation wasn't even attempted, eg. due to missing data.
      */
     MigrationStatus transform(String uuid) throws Exception;
 
